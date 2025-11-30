a414524a6f72451c8bbd5bf6852f3cc3266c1d14
SumitC962 <sumitchaudhari079@gmail.com>
2025-11-30 13:12:30 +0530

    30-11-2025

---
+++ Leetcode_2025/1590_Make_Sum_Divisible_by_P_Solve.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long long totalSum = 0;
+        for (int x : nums) totalSum += x;
+        int need = totalSum % p;
+        if (need == 0) return 0;
+
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+
+        long long prefix = 0;
+        int ans = nums.size();
+
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            prefix = (prefix + nums[i]) % p;
+            int target = (prefix - need + p) % p;
+
+            if (mp.count(target)) {
+                ans = min(ans, i - mp[target]);
+            }
+
+            mp[prefix] = i;
+        }
+
+        return ans == (int)nums.size() ? -1 : ans;
+    }
+};
+
+// Minimal `main` so the file is easy to build/run in VS Code.
+// Change the sample input below or replace with a simple parser.
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Sample test
+    vector<int> nums = {3, 1, 4, 2};
+    int p = 6;
+    Solution sol;
+    cout << sol.minSubarray(nums, p) << '\n';
+    return 0;
+}
